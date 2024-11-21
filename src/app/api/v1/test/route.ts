cc1291a67f30cb6b690fe2ac67a7b4356ecc3c51
KagnSeohyeon <shkang@coinplug.com>
2024-11-21 10:12:48 +0900

    난이도 1 ~ 3은 인지, 4 ~ 5는 불명확 및 2 이후는 답변을 안주려고 함

--- src/app/api/v1/test/route.ts
+++ src/app/api/v1/test/route.ts
@@ -6,8 +6,8 @@ export async function GET() {
   try {
     const fineTuneModels = await listFineTuneModels();
     console.log(fineTuneModels.data[0].fine_tuned_model);
-    const embeddings1 = await requestEmbedding("병합 정렬");
-    const embeddings2 = await requestEmbedding("난이도 쉬움");
+    const embeddings1 = await requestEmbedding("정렬 알고리즘을 사용한 난이도 1 문제를 출제해줘");
+    const embeddings2 = await requestEmbedding("정렬 알고리즘을 사용한 난이도 2 문제를 출제해줘");
 
     if (embeddings1 && embeddings2) {
       const similarity = cosineSimilarity(
